e905d5eb00546382e8a9e81900cab0cbfd5eaacc
Diego Edgar <43714681+diegoedgar69@users.noreply.github.com>
2021-03-03 14:28:06 -0600

    1.2.1
    coreecion de var

--- utilidades/js/apiearthquake.js
+++ utilidades/js/apiearthquake.js
@@ -1,14 +1,14 @@
 var fecha = new Date();
-var horamenos = fecha.getFullYear() + '-' + (fecha.getMonth() +1)+ '-' + fecha.getDay() + ' ' + (fecha.getHours() -1)+ ':' + fecha.getMinutes() + ':' + fecha.getSeconds() + '-06:00';
+var horamenos = fecha.getUTCFullYear() + '-' + (fecha.getUTCMonth() + 1) + '-' + fecha.getUTCDay() + ' ' + (fecha.getUTCHours() - 1) + ':' + fecha.getUTCMinutes() + ':' + fecha.getUTCSeconds();
 var ur = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + horamenos;
 $.ajax({
     url: ur,
     dataType: 'json',
     success: function (data) {
         var caja = document.getElementById('caja');
-        var text = 'lugar, magnitud, coordenadas, horas<br>';
+        var text = ur + '<br>lugar, magnitud, coordenadas, horas<br>';
         const tamanio = Object.keys(data.features).length;
-        for (var i = 1; i <= tamanio - 1; i++) {
+        for (var i = 0; i <= tamanio - 1; i++) {
             var dia = new Date(data.features[i].properties.time);
             text += data.features[i].properties.place + ', ' + data.features[i].properties.mag + ', ' + data.features[i].geometry.coordinates + ', ' + dia.toString() + '<br>'
         }
