4bc0fc0b7d7e0b5222937dfed43e842a13fc4010
Diego Edgar <43714681+diegoedgar69@users.noreply.github.com>
2021-03-03 00:17:34 -0600

    1
    test

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="icon" type="image/png" href="utilidades/imagenes/favicon.png">
+    <meta charset="utf-8">
+    <title>Earthquake</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+    <link rel="stylesheet" href="utilidades/css/bootstrap.min.css">
+    <script src="utilidades/js/jquery-3.5.1.js"></script>
+</head>
+<body>
+    <div id="caja">
+    </div>
+    <script src="utilidades/js/jquery-3.5.1.js"></script>
+    <script src="utilidades/js/bootstrap.min.js"></script>
+    <script src="utilidades/js/apiearthquake.js"></script>
+</body>
+</html>
